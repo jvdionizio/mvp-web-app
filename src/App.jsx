c3a66b00cb302ce69d69ea58dbf084fbc70b609e
Gabriel Altemeyer <gabriel.altemeyer@gmail.com>
2022-11-04 21:48:23 -0300

    feat: Products Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound'
+import Products from './pages/Products';
+
 
 function App() {
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/*" element={<NotFound />} />
+          <Route exact path="/" element={<Home />}/>
+          <Route exact path="/products" element={<Products />}/>
+          <Route exact path="/*" element={<NotFound />} />
         </Routes>
       </div>
     </Router>
