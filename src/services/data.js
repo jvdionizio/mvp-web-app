07b86e3c1bebd728698e188df3e77927fbedf8b2
Gabriel Altemeyer <gabriel.altemeyer@gmail.com>
2022-11-08 21:00:35 -0300

    feat: products model

---
+++ src/services/data.js
@@ -0,0 +1,22 @@
+const products = {
+    headsets: [
+        {
+            name: 'Headset',
+            value: 900.00,
+            description: "muito gamers",
+            evaluation: 2,
+            urls: [
+                "link",
+                "link2",
+            ]
+        },
+        {
+            name: 'Headset',
+            value: 900.00,
+            urls: [
+                "link",
+                "link2",
+            ]
+        }
+    ],
+}
