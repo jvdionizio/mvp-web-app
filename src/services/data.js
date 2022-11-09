45aec5495de33813f7c47d81e23bbcc9be8b8337
Beatriz Avelino <beatrizavelinodasilva31@gmail.com>
2022-11-08 22:01:02 -0300

    feat: new products

--- src/services/data.js
+++ src/services/data.js
@@ -11,12 +11,146 @@ const products = {
             ]
         },
         {
-            name: 'Headset',
-            value: 900.00,
+            name: 'Headset gamer premium com som surround 7.1 VOID RGB ELITE USB - Branco',
+            value: 999.90,
+            description: "muito gamers",
+            evaluation: 4,
+            brand: corsair,
             urls: [
                 "link",
                 "link2",
             ]
-        }
+        },
+        {
+            name: 'Headset Gamer Sem Fio Corsair HS80 Premium',
+            value: 999.90,
+            description: "muito gamers",
+            evaluation: 4,
+            brand: corsair,
+            urls: [
+                "https://images.kabum.com.br/produtos/fotos/216705/headset-gamer-sem-fio-corsair-hs80-premium-rgb-surround-dolby-atmos-wireless-drivers-50mm-preto-ca-9011235-na_1636559754_gg.jpg",
+                "link2",
+            ]
+        },
+        {
+            name: 'HyperX Cloud Orbit S - Headset gamer',
+            value: 1399.00,
+            description: 'um fone mt bom, muito gaymer, muito incirvel',
+            evaluation: 5,
+            brand: hyperx,
+            urls: [
+                "https://images.kabum.com.br/produtos/fotos/105006/headset-hyperx-cloud-orbit-s-audio-3d-hx-hscos-gm-ww_headset-hyperx-cloud-orbit-s-audio-3d-hx-hscos-gm-ww_1571342913_gg.jpg",
+                "link2",
+            ]
+        },
+        {
+            name: 'Headset Gamer HyperX Cloud Stinger Core',
+            value: 156.99,
+            description: 'um fone mt bom, muito gaymer, muito incirvel',
+            evaluation: 5,
+            brand: hyperx,
+            urls: [
+                "https://images.kabum.com.br/produtos/fotos/97020/97020_2_1528399780_gg.jpg",
+                "link2",
+            ]
+        },
+        {
+            name: 'Headset Gamer HyperX Cloud Stinger Core',
+            value: 156.99,
+            description: 'um fone mt bom, muito gaymer, muito incirvel',
+            evaluation: 5,
+            brand: hyperx,
+            urls: [
+                "https://images.kabum.com.br/produtos/fotos/97020/97020_2_1528399780_gg.jpg",
+                "link2",
+            ]
+        },
+        {
+            name: 'Headset Gamer HyperX Cloud Stinger Core',
+            value: 156.99,
+            description: 'um fone mt bom, muito gaymer, muito incirvel',
+            evaluation: 5,
+            brand: hyperx,
+            urls: [
+                "https://images.kabum.com.br/produtos/fotos/97020/97020_2_1528399780_gg.jpg",
+                "link2",
+            ]
+        },
+        {
+            name: 'Headset Gamer HyperX Cloud Stinger Core',
+            value: 156.99,
+            description: 'um fone mt bom, muito gaymer, muito incirvel',
+            evaluation: 5,
+            brand: hyperx,
+            urls: [
+                "https://images.kabum.com.br/produtos/fotos/97020/97020_2_1528399780_gg.jpg",
+                "link2",
+            ]
+        },
+        {
+            name: 'Headset Gamer HyperX Cloud Stinger Core',
+            value: 156.99,
+            description: 'um fone mt bom, muito gaymer, muito incirvel',
+            evaluation: 5,
+            brand: hyperx,
+            urls: [
+                "https://images.kabum.com.br/produtos/fotos/97020/97020_2_1528399780_gg.jpg",
+                "link2",
+            ]
+        },
+        
+    
     ],
+    mouses: [
+        {
+            name: 'HyperX Pulsefire Core Mouse para Jogos, Preto, Até 6200 dpi',
+            value: 99.99,
+            description: "Fácil personalização com o software HyperX NGenuity - Crie macros e personalize a iluminação RGB e DPI. Design ergonômico e confortável - Projetado de forma ergonômica para se adaptar à sua mão e com apoios laterais texturizados para permanecer firme durante movimentos rápidos. Sete botões programáveis e personalizáveis – O Pulsefire Core conta com sete botões programáveis com switches de nível gamer proporcionam nítido feedback tátil configuradas para 20 milhões de cliques",
+            evaluation: 5,
+            brand: 'HyperX',
+            urls: [
+                "https://m.media-amazon.com/images/I/413jb9SWLPS._AC_SL1000_.jpg",
+                "link2https://m.media-amazon.com/images/I/61ZwGbQR+OL._AC_SL1428_.jpg",
+                "https://m.media-amazon.com/images/I/6172qm9x6ZL._AC_SL1428_.jpg",
+                "https://m.media-amazon.com/images/I/61v3ghfsCZL._AC_SL1428_.jpg",
+                "https://m.media-amazon.com/images/I/610AZ15CkdL._AC_SL1428_.jpg",
+                "https://m.media-amazon.com/images/I/719VjjkA-KL._AC_SL1428_.jpg",
+                "https://m.media-amazon.com/images/I/51MqrF0RKoL._AC_SL1428_.jpg",
+                "https://m.media-amazon.com/images/I/61MEI0G3OfS._AC_SL1500_.jpg",
+            ]
+            
+        }
+        
+    ],
+    
+    keyboard: [
+        {
+            name: 'Teclado Gamer Razer Cynosa Chroma RGB, Windows',
+            value: 299.00,
+            description: "muito teclavel",
+            evaluation: 4,
+            brand: 'razer', 
+            urls: [
+                "link",
+                "link2",
+            ]
+
+        }
+        
+    ],
+    
+    case:[
+        {
+            name: 'Gabinete Gamer Thermaltake Versa H18 TG RGB, ATX, 2x USB 3.0, 1x USB 2.0, 1x Audio, 1x Microfone, 1x RGB, Preto',
+            value: 700.00, 
+            description: "muito gabinavel",
+            evaluation: 4,
+            brand: 'thermaltake',
+            urls: [
+                "link",
+                "link2",
+            ]
+            
+        }
+    ]
 }
