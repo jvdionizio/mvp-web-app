a6f1ac08fd6663185b4be66beb0a9e9d3113f564
Beatriz Avelino <beatrizavelinodasilva31@gmail.com>
2022-11-27 11:28:53 -0300

    Fix: Details, Font sizes, spacing,

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,18 +1,18 @@
 /* eslint-disable react/jsx-max-depth */
-import React, { useContext, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import { Star } from 'phosphor-react';
-import { clsx } from 'clsx';
-import Text from '../components/styles/Text';
-import Footer from '../components/Footer';
-import Context from '../context/Context';
-import Header from '../components/Header';
-import Button from '../components/styles/Button';
+import React, { useContext, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { Star } from "phosphor-react";
+import { clsx } from "clsx";
+import Text from "../components/styles/Text";
+import Footer from "../components/Footer";
+import Context from "../context/Context";
+import Header from "../components/Header";
+import Button from "../components/styles/Button";
 
 function ProductDetails() {
   const { allProducts } = useContext(Context);
   const { pathname } = useLocation();
-  const pathName = pathname.split('/')[2];
+  const pathName = pathname.split("/")[2];
   const id = Number(pathName);
   const product = allProducts.find((item) => item.id === id);
   console.log(product);
@@ -30,8 +30,8 @@ function ProductDetails() {
   return (
     <div className="w-full bg-white">
       <Header />
-      { product && (
-        <div className="flex flex-row w-auto mt-36 mx-32 wx-52 wy-11">
+      {product && (
+        <div className="flex flex-row mt-32 mx-32"> {/*flex flex-row mt-36 mx-32 wx-52 wy-11*/}
           <div
             className="
           border-2
@@ -42,50 +42,60 @@ function ProductDetails() {
         "
           >
             <img
-              className=""
-              alt={ `foto do produto ${product.name}` }
-              src={ product.urls[0] }
+              className="object-contain"
+              alt={`foto do produto ${product.name}`}
+              src={product.urls[0]}
             />
           </div>
           <span className="w-full mt-16">
-            <div className="w-full flex flex-col">
+            <div className="w- flex flex-col">
               <div className="flex flex-row" />
+              <div className="flex flex-row">
+                <p className="text-sm pr-2 text-purple-500 pb-2">4 de 5</p>
+                <Star size={27} color="#6E55CA" weight="fill" />
+                <Star size={27} color="#6E55CA" weight="fill" />
+                <Star size={27} color="#6E55CA" weight="fill" />
+                <Star size={27} color="#6E55CA" weight="fill" />
+                <Star size={27} color="#6E55CA" weight="regular" />
+              </div>
               <Text asChild>
-                <p className="text-md font-semibold p-2">{product.name}</p>
+                <p className="text-2md font-semibold py-2">{product.name}</p>
               </Text>
-              <Text asChild>
-                <p className="text-purple-300 font-semibold text-lg p-2">
-                  R$
-                  {product.value}
+              <Text>
+                <p className="text-purple-300 font-ubuntu font-semibold text-lg py-2">
+                  R${product.value}
                 </p>
               </Text>
-              {/* to do disponibilidade e garantia */}
               <div className="flex flex-col">
                 <Text asChild>
-                  <p className="font-ubuntu font-light">Disponibilidade: Imediata</p>
+                  <p className="font-ubuntu text-sm pt-2">
+                    Disponibilidade: Imediata
+                  </p>
                 </Text>
                 <Text asChild>
-                  <p className="pt-2 pb-2">Garantia: 2 anos</p>
+                  <p className="pb-3 font-ubuntu">Garantia: 2 anos</p>
                 </Text>
               </div>
               <Text asChild>
                 <p>10x de R$99,99 sem juros ou à vista no PIX 10% OFF</p>
               </Text>
-              <div className="flex flex-row items-center p-2 border-">
+              <div className="flex flex-row items-center">
                 <Text asChild>
                   <p>Quantidade</p>
                 </Text>
-                <div className="flex flex-row items-center bg-yellow">
+                <div className="flex flex-row items-center">
+                  <div>
                   <Button asChild color="black" bgColor="transparent">
-                    <button type="button" onClick={ decrease }>
+                    <button type="button" onClick={decrease}>
                       -
                     </button>
                   </Button>
-                  <div>
+                  </div>
+                  <div className= "text-center">
                     <p>{amount}</p>
                   </div>
                   <Button asChild color="black" bgColor="transparent">
-                    <button type="button" onClick={ increase }>
+                    <button type="button" onClick={increase}>
                       +
                     </button>
                   </Button>
@@ -93,21 +103,23 @@ function ProductDetails() {
               </div>
               <button
                 type="button"
-                className={ clsx(
-                  'py-4',
-                  'px-3',
-                  'bg-cyan-500 rounded',
-                  'font-bowlby',
-                  'font-semibold',
-                  'tracking-tighter',
-                  'text-white-smoked',
-                  'text-sm w-full',
-                  'transition-colors',
-                  'hover:bg-cyan-300',
-                  'hover:outline-none',
-                  'w-36',
-                  'h-12',
-                ) }
+                className={clsx(
+                  "py-4",
+                  "px-3",
+                  "bg-cyan-500 rounded",
+                  "font-bowlby",
+                  "font-semibold",
+                  "tracking-tighter",
+                  "text-white-smoked",
+                  "text-sm w-full",
+                  "transition-colors",
+                  "hover:bg-cyan-300",
+                  "hover:outline-none",
+                  "w-36",
+                  "h-12",
+                  "justify-betweenn",
+                  "items-center",
+                )}
               >
                 COMPRAR
               </button>
@@ -115,8 +127,7 @@ function ProductDetails() {
           </span>
           <div className="w-full flex gap-16 m-auto" />
         </div>
-      )}
-      {' '}
+      )}{" "}
       {!product && (
         <div>
           <div className="w-full my-72 flex flex-col text-center text-lg font-semibold">
@@ -124,18 +135,18 @@ function ProductDetails() {
           </div>
         </div>
       )}
-      {
-        product && (
-          <div className="flex flex-col w-full mx-32 bg-gray-400">
+      {product && (
+        <div className="flex flex-col w-5/6 mx-32 pb-8">
+          <Text asChild>
+            <p className="pb-2 underline">DETALHES</p>
+          </Text>
+          <div className="bg-gray-100">
             <Text asChild>
-              <p>DETALHES</p>
+              <p>{product.description}</p>
             </Text>
-            <div className="bg-gray-200">
-              <Text asChild><p>{product.description}</p></Text>
-            </div>
           </div>
-        )
-      }
+        </div>
+      )}
       <Footer />
     </div>
   );
